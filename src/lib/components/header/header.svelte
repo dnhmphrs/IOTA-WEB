8fdbbf837ae0c137bb3868954f35d2c1287d1508
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-25 14:11:07 -0500

    update

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -73,7 +73,7 @@
 		user-select: none;
 		color: var(--primary);
 		border-bottom: solid 1px var(--primary-50);
-		background: var(--background-header);
+		background: var(--background);
 		box-shadow: 0px -1px 8px #23232320;
 	}
 
