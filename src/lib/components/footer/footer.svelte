9012c57449e89e899f76c9de289b60eee2405476
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-24 22:40:44 -0500

    edit mobile footer

--- src/lib/components/footer/footer.svelte
+++ src/lib/components/footer/footer.svelte
@@ -2,25 +2,32 @@
 </script>
 
 <footer>
-	<p class="xl">iOTA</p>
 	<a href="https://www.websitecarbon.com/website/iota-health/">0.04G CO2 / VISIT</a>
-	<p class="">COPYRIGHT 2022</p>
+	<p class="xl">COPYRIGHT 2022</p>
 </footer>
 
 <style>
 	footer {
 		width: 100%;
-		height: 60px;
+		min-height: 60px;
 		z-index: 10;
+		gap: 10px;
 
-		padding: 0 40px;
+		padding: 20px 40px;
 		background: var(--background-alt);
 		border-top: solid 1px var(--accent-50);
 		text-transform: none;
 		color: var(--accent-50);
 
 		display: flex;
+		flex-flow: row nowrap;
 		align-items: center;
 		justify-content: space-between;
+	}
+
+	@media only screen and (max-width: 768px) {
+		footer {
+			flex-flow: column nowrap;
+		}
 	}
 </style>
