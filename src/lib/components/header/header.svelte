1d493d9cfa25a7854162bf25226ff329efa6f3f1
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-25 17:52:40 -0500

    remove color

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -71,7 +71,7 @@
 		height: 60px;
 
 		user-select: none;
-		color: var(--toggle);
+		color: var(--primary);
 		border-bottom: solid 1px var(--primary-50);
 		background: var(--background);
 		box-shadow: 0px -1px 8px #23232320;
