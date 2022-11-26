478de16c3025bee2e95926885573d20f22980b26
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-25 19:06:00 -0500

    update

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -1,5 +1,4 @@
 import { writable } from 'svelte/store';
 
 export const screenType = writable(null);
-export const darkMode = writable(true);
-export const sectionId = writable('iota');
+export const darkMode = writable(false);
