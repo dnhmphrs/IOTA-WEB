c001793b35b5d7674b47c47ebf8f3deddfdac068
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2022-11-25 20:45:20 -0500

    update

--- src/routes/(page)/+page.svelte
+++ src/routes/(page)/+page.svelte
@@ -12,7 +12,7 @@
 
 	let loremShort =
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut \
-	// 	labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco';
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco';
 </script>
 
 <main>
