01f31fa4fff2c8b4f63d2e57d3cfb87c3589ca4d
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2023-02-26 19:42:28 +0000

    edit

--- src/lib/store/store.js
+++ src/lib/store/store.js
@@ -2,4 +2,4 @@ import { writable } from 'svelte/store';
 
 export const screenType = writable(null);
 export const darkMode = writable(false);
-export const isIframe = writable(true);
+export const isIframe = writable(false);
